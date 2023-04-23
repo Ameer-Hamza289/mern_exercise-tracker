c6f83c2b5378d2c6c26d19c25745702dbe57338f
Ameer Hamza <ameer.hamza102002@gmail.com>
2023-04-23 19:02:19 +0500

    Tracker

---
+++ src/Components/EditExercise.js
@@ -0,0 +1,131 @@
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+
+const EditExercise = (props) => {
+  const [username, setUsername] = useState('');
+  const [description, setDescription] = useState('');
+  const [duration, setDuration] = useState(0);
+  const [date, setDate] = useState(new Date());
+  const [users, setUsers] = useState([]);
+
+  const userInputRef = useRef(null);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/exercises/' + props.match.params.id)
+      .then(response => {
+        setUsername(response.data.username);
+        setDescription(response.data.description);
+        setDuration(response.data.duration);
+        setDate(new Date(response.data.date));
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+
+    axios.get('http://localhost:5000/users/')
+      .then(response => {
+        if (response.data.length > 0) {
+          setUsers(response.data.map(user => user.username));
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+
+    userInputRef.current.focus();
+  }, [props.match.params.id]);
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  }
+
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
+  }
+
+  const handleDurationChange = (e) => {
+    setDuration(e.target.value);
+  }
+
+  const handleDateChange = (date) => {
+    setDate(date);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const exercise = {
+      username: username,
+      description: description,
+      duration: duration,
+      date: date
+    }
+
+    console.log(exercise);
+
+    axios.post('http://localhost:5000/exercises/update/' + props.match.params.id, exercise)
+      .then(res => console.log(res.data));
+
+    window.location = '/';
+  }
+
+  return (
+    <div>
+      <h3>Edit Exercise Log</h3>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Username: </label>
+          <select ref={userInputRef}
+            required
+            className="form-control"
+            value={username}
+            onChange={handleUsernameChange}>
+            {
+              users.map(function (user) {
+                return <option
+                  key={user}
+                  value={user}>{user}
+                </option>;
+              })
+            }
+          </select>
+        </div>
+        <div className="form-group">
+          <label>Description: </label>
+          <input type="text"
+            required
+            className="form-control"
+            value={description}
+            onChange={handleDescriptionChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Duration (in minutes): </label>
+          <input
+            type="text"
+            className="form-control"
+            value={duration}
+            onChange={handleDurationChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Date: </label>
+          <div>
+            <DatePicker
+              selected={date}
+              onChange={handleDateChange}
+            />
+          </div>
+        </div>
+
+        <div className="form-group">
+          <input type="submit" value="Edit Exercise Log" className="btn btn-primary" />
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default EditExercise;
